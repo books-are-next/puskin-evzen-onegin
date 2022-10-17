2a67c3b138dd2465c7050993d7ed12c201ee4745
gabGodiskova <gabgodiskova@gmail.com>
2022-10-17 18:15:36 +0000

    Deploying to docs from @ books-are-next/puskin-evzen-onegin@350b0e32d7be9510e49ed7ca0a2488a011d1f862 🚀

--- epub2nb-output/content/about.md
+++ epub2nb-output/content/about.md
@@ -9,7 +9,8 @@ Překlad Josef Hora
 Redakce Jaroslava Bednářová  
 Vydala **Městská knihovna v Praze**  
 Mariánské nám. 1, 115 72 Praha 1  
-V MKP 2. elektronické vydání z 10. 10. 2022.
+ISBN 978-80-274-2843-4 (webová kniha)  
+V MKP 2. elektronické vydání z 17. 10. 2022.
 
 ***
 
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-f112a46';
+const CACHE = 'cache-350b0e3';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
